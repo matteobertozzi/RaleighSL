47e90bb44daaa5e90d2eab9583f07007e0214433
Matteo Bertozzi <theo.bertozzi@gmail.com>
2011-12-25 19:10:22 +0100

    added more raleighfs struct size dump

--- tests/raleighfs/struct-sizes.c
+++ tests/raleighfs/struct-sizes.c
@@ -5,11 +5,13 @@
 #define __usizeof(x)                ((unsigned int)sizeof(x))
 
 int main (int argc, char **argv) {
-    printf("raleighfs_t         %u\n", __usizeof(raleighfs_t));
-    printf("raleighfs_key_t     %u\n", __usizeof(raleighfs_key_t));
-    printf("raleighfs_master_t  %u\n", __usizeof(raleighfs_master_t));
-    printf("raleighfs_object_t  %u\n", __usizeof(raleighfs_object_t));
-    printf("raleighfs_objdata_t %u\n", __usizeof(raleighfs_objdata_t));
+    printf("raleighfs_t          %u\n", __usizeof(raleighfs_t));
+    printf("raleighfs_key_t      %u\n", __usizeof(raleighfs_key_t));
+    printf("raleighfs_rwlock_t   %u\n", __usizeof(raleighfs_rwlock_t));
+    printf("raleighfs_device_t   %u\n", __usizeof(raleighfs_device_t));
+    printf("raleighfs_master_t   %u\n", __usizeof(raleighfs_master_t));
+    printf("raleighfs_object_t   %u\n", __usizeof(raleighfs_object_t));
+    printf("raleighfs_objdata_t  %u\n", __usizeof(raleighfs_objdata_t));
     return(0);
 }
 
